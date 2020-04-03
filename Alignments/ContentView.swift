dee743dcacdad3ef60b17c4a5db7653a5436f5bd
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-03 09:31:48 +0300

    init

--- Alignments/ContentView.swift
+++ Alignments/ContentView.swift
@@ -10,7 +10,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            VStack(alignment: .trailing) {
+                Text("Username")
+                    //.border(Color.red)
+                Text("Email")
+                    //.border(Color.red)
+                Text("Phone")
+                    //.border(Color.red)
+            }.font(Font.system(size: 35, weight: .bold))
+            
+            VStack(alignment: .leading) {
+                Text("Jack")
+                    .frame(maxHeight: .infinity)
+                    //.border(Color.red)
+                Text("jack@mail.com")
+                    .frame(maxHeight: .infinity)
+                    //.border(Color.red)
+                Text("9867564312")
+                    .frame(maxHeight: .infinity)
+                    //.border(Color.red)
+            }.font(.system(size: 25))
+             
+        }.fixedSize()
     }
 }
 
